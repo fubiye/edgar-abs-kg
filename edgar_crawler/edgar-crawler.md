1cb089fbbdf30780bf591f4d341df762fc7400f9
fubiye <biye.fu@outlook.com>
2021-05-16 17:10:00 +0800

    Add EDGAR Companies Crawler

---
+++ edgar_crawler/edgar-crawler.md
@@ -0,0 +1,15 @@
+# EDGAR Crawler
+
+Data crawler from SEC EDGAR System
+
+## Project
+
+Create a scrapy project
+```
+scrapy startproject edgar_crawler
+```
+
+## Start Crawler
+```
+scrapy crawl companies
+```
---
+++ edgar_crawler/edgar_crawler/spiders/companies_spider.py
@@ -0,0 +1,30 @@
+from edgar_crawler.items import CompanyItem
+import scrapy
+
+class CompaniesSpider(scrapy.Spider):
+    name = "companies"
+    
+    def start_requests(self):
+        urls = [
+            'https://www.sec.gov/cgi-bin/browse-edgar?company=&match=&filenum=&State=&Country=&SIC=6189&myowner=exclude&action=getcompany'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        url = response.request.url
+        print("Start parsing data from page: {}".format(url))
+        
+        tblRowEles = response.xpath("//div[@id='seriesDiv']/table//tr")
+        for rowEle in tblRowEles:
+            colEles = rowEle.xpath("./td//text()")
+            if len(colEles) < 3:
+                continue
+            company = CompanyItem()
+            company['sic'] = url[url.index('&SIC=')+len('&SIC='): url.index("&myowner")]
+            company['cik'] = colEles[0].extract()
+            company['company'] = colEles[1].extract()
+            company['loc'] = colEles[2].extract()
+
+            print(company)
+        pass
